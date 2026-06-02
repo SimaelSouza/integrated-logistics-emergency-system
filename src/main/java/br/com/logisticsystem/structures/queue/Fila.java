14ce26ffee66ca1171cc6ad53e5834b974fd6bc8
Arione-gomes <arionegomes96@gmail.com>
2026-06-02 20:30:32 -0300

    FEAT: adiciona classe fila simples de atendimento

--- src/main/java/br/com/logisticsystem/structures/queue/Fila.java
+++ src/main/java/br/com/logisticsystem/structures/queue/Fila.java
@@ -1,4 +1,31 @@
 package br.com.logisticsystem.structures.queue;
 
+import br.com.logisticsystem.models.Atendimento;
+
 public class Fila {
+    private int capacidade;
+    private Atendimento[] atendimentos;
+    public Fila(int capacidade) {
+        this.capacidade=capacidade;
+        this.atendimentos = new Atendimento[capacidade];
+    }
+    public void visualizaFila(){
+        for (int i =0 ; i<capacidade; i++){
+            if(atendimentos[i] != null){
+            System.out.println(atendimentos[i]);
+            } else {continue;}
+        }
+    }
+    public void remocao(){
+        for (int i =0; i <capacidade; i ++){
+            if(atendimentos[i] != null){
+                atendimentos[i]=null;
+                break;
+            }
+        }
+    }
+
+
+
+
 }
