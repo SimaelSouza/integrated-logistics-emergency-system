935447dc21d7d4a48214fb3ca61c77affc081fff
SimaelSouza <simaelsouza2019@gmail.com>
2026-06-05 16:36:17 -0300

    test: configura execução de testes com JUnit 5

--- src/test/java/queue/FilaTest.java
+++ src/test/java/queue/FilaTest.java
@@ -1,4 +1,13 @@
 package queue;
 
-public class FilaTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FilaTest {
+
+    @Test
+    void deveExecutarTesteComJUnit5() {
+        assertTrue(true);
+    }
 }
