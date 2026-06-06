6bc759abbdce6c7d10f38d93822d13dba010e980
SimaelSouza <simaelsouza2019@gmail.com>
2026-06-06 19:24:52 -0300

    feature: adiciona testes e um esqueleto para a classe Fila

--- src/main/java/br/com/logisticsystem/structures/queue/Fila.java
+++ src/main/java/br/com/logisticsystem/structures/queue/Fila.java
@@ -1,4 +1,34 @@
 package br.com.logisticsystem.structures.queue;
 
+import br.com.logisticsystem.models.Atendimento;
+
 public class Fila {
+
+    public Fila(int capacidade) {}
+
+    public void enqueue(Atendimento atendimento) {}
+
+    public Atendimento dequeue() {
+        return null;
+    }
+
+    public Atendimento peek() {
+        return null;
+    }
+
+    public Atendimento[] listar() {
+        return null;
+    }
+
+    public int getTamanho() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
 }
