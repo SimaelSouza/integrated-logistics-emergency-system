542944b54845b8896d14c16599311ccc48fcc3a0
SimaelSouza <simaelsouza2019@gmail.com>
2026-06-11 16:21:38 -0300

    feat: adiciona um esqueleto para as classes ListaSimples e NoSimples

--- src/main/java/br/com/logisticsystem/structures/singlylinkedlist/NoSimples.java
+++ src/main/java/br/com/logisticsystem/structures/singlylinkedlist/NoSimples.java
@@ -1,4 +1,27 @@
 package br.com.logisticsystem.structures.singlylinkedlist;
 
-public class NoSimples {
+public class NoSimples<T> {
+
+    private T dado;
+    private NoSimples<T> proximo;
+
+    public NoSimples(T dado) {
+        this.dado = dado;
+    }
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    public NoSimples<T> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NoSimples<T> proximo) {
+        this.proximo = proximo;
+    }
 }
