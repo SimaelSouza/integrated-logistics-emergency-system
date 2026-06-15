6cba6090d650d942aa767b9d364ea9f8ddee7756
simael <simaelsouza2019@gmail.com>
2026-06-15 13:13:52 -0300

    feat: adiciona o esqueleto da ListaDupla e NoDuplo para implementação por TDD

--- src/main/java/br/com/logisticsystem/structures/doublylinkedlist/ListaDupla.java
+++ src/main/java/br/com/logisticsystem/structures/doublylinkedlist/ListaDupla.java
@@ -1,4 +1,49 @@
 package br.com.logisticsystem.structures.doublylinkedlist;
 
-public class ListaDupla {
+import br.com.logisticsystem.exceptions.PosicaoInvalidaException;
+
+public class ListaDupla<T> {
+
+    private NoDuplo<T> primeiro;
+    private NoDuplo<T> ultimo;
+    private int tamanho;
+
+    public ListaDupla() {
+        this.primeiro = null;
+        this.ultimo = null;
+        this.tamanho = 0;
+    }
+
+    public void inserir(T elemento) {
+
+    }
+
+    public T remover(int posicao) {
+        return null;
+    }
+
+    public T buscar(int posicao) {
+        return null;
+    }
+
+    public String listarFrente() {
+        return "";
+    }
+
+    public String listarTras() {
+        return "";
+    }
+
+    public boolean isEmpty() {
+        return tamanho == 0;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    private void validarPosicao(int posicao) throws PosicaoInvalidaException {
+
+    }
+
 }
--- src/main/java/br/com/logisticsystem/structures/doublylinkedlist/NoDuplo.java
+++ src/main/java/br/com/logisticsystem/structures/doublylinkedlist/NoDuplo.java
@@ -1,4 +1,37 @@
 package br.com.logisticsystem.structures.doublylinkedlist;
 
-public class NoDuplo {
+public class NoDuplo<T> {
+
+    private T dado;
+    private NoDuplo<T> anterior;
+    private NoDuplo<T> proximo;
+
+    public NoDuplo(T dado) {
+        this.dado = dado;
+    }
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    public NoDuplo<T> getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NoDuplo<T> anterior) {
+        this.anterior = anterior;
+    }
+
+    public NoDuplo<T> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NoDuplo<T> proximo) {
+        this.proximo = proximo;
+    }
+
 }
