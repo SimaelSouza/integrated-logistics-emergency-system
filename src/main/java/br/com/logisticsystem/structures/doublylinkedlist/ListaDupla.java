31eb1c3b9d9aff9ac352cbd951cb2b41c76a7aa5
Arione-gomes <arionegomes96@gmail.com>
2026-06-16 10:35:36 -0300

    feat: acrescenta as classes NoDuplo e ListaDupla

--- src/main/java/br/com/logisticsystem/structures/doublylinkedlist/ListaDupla.java
+++ src/main/java/br/com/logisticsystem/structures/doublylinkedlist/ListaDupla.java
@@ -1,49 +1,168 @@
 package br.com.logisticsystem.structures.doublylinkedlist;
 
-import br.com.logisticsystem.exceptions.PosicaoInvalidaException;
-
 public class ListaDupla<T> {
-
-    private NoDuplo<T> primeiro;
-    private NoDuplo<T> ultimo;
+    private NoDuplo<T> inicio;
+    private NoDuplo<T> fim;
     private int tamanho;
 
     public ListaDupla() {
-        this.primeiro = null;
-        this.ultimo = null;
+        this.inicio = null;
+        this.fim = null;
         this.tamanho = 0;
     }
 
-    public void inserir(T elemento) {
 
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
+
+    public boolean isEmpty() {
+        return this.tamanho == 0;
+    }
+
+    private void validarPosicao(int posicao) {
+        if (posicao < 0 || posicao >= this.tamanho) {
+            throw new IndexOutOfBoundsException("Posição inválida: " + posicao);
+        }
+    }
+
+    private void validarPosicaoInsercao(int posicao) {
+        if (posicao < 0 || posicao > this.tamanho) {
+            throw new IndexOutOfBoundsException("Posição de inserção inválida: " + posicao);
+        }
+    }
+
+
+    private NoDuplo<T> obterNo(int posicao) {
+        validarPosicao(posicao);
+
+        NoDuplo<T> atual;
+        if (posicao < this.tamanho / 2) {
+            atual = this.inicio;
+            for (int i = 0; i < posicao; i++) {
+                atual = atual.getProximo();
+            }
+        } else {
+            atual = this.fim;
+            for (int i = this.tamanho - 1; i > posicao; i--) {
+                atual = atual.getAnterior();
+            }
+        }
+        return atual;
+    }
+
+
+    public T buscar(int posicao) {
+        return obterNo(posicao).getElemento();
+    }
+
+
+    public void inserir(T elemento) {
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        if (isEmpty()) {
+            this.inicio = novoNo;
+            this.fim = novoNo;
+        } else {
+            this.fim.setProximo(novoNo);
+            novoNo.setAnterior(this.fim);
+            this.fim = novoNo;
+        }
+        this.tamanho++;
+    }
+
+
+    public void inserir(int posicao, T elemento) {
+        validarPosicaoInsercao(posicao);
+
+        if (posicao == 0) {
+            NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+            if (isEmpty()) {
+                this.inicio = novoNo;
+                this.fim = novoNo;
+            } else {
+                novoNo.setProximo(this.inicio);
+                this.inicio.setAnterior(novoNo);
+                this.inicio = novoNo;
+            }
+            this.tamanho++;
+        } else if (posicao == this.tamanho) {
+            inserir(elemento);
+        } else {
+            NoDuplo<T> noAtual = obterNo(posicao);
+            NoDuplo<T> noAnterior = noAtual.getAnterior();
+            NoDuplo<T> novoNo = new NoDuplo<>(elemento, noAtual, noAnterior);
+
+            noAnterior.setProximo(novoNo);
+            noAtual.setAnterior(novoNo);
+            this.tamanho++;
+        }
     }
 
     public T remover(int posicao) {
-        return null;
+        validarPosicao(posicao);
+
+        NoDuplo<T> noAlvo = obterNo(posicao);
+        T elementoRemovido = noAlvo.getElemento();
+
+        if (posicao == 0) {
+            this.inicio = noAlvo.getProximo();
+            if (this.inicio != null) {
+                this.inicio.setAnterior(null);
+            } else {
+                this.fim = null;
+            }
+        } else if (posicao == this.tamanho - 1) {
+            this.fim = noAlvo.getAnterior();
+            if (this.fim != null) {
+                this.fim.setProximo(null);
+            } else {
+                this.inicio = null;
+            }
+        } else {
+            noAlvo.getAnterior().setProximo(noAlvo.getProximo());
+            noAlvo.getProximo().setAnterior(noAlvo.getAnterior());
+        }
+
+        this.tamanho--;
+        return elementoRemovido;
     }
 
-    public T buscar(int posicao) {
-        return null;
-    }
 
     public String listarFrente() {
-        return "";
+        if (isEmpty()) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        NoDuplo<T> atual = this.inicio;
+        while (atual != null) {
+            sb.append(atual.getElemento().toString());
+            if (atual.getProximo() != null) {
+                sb.append(", ");
+            }
+            atual = atual.getProximo();
+        }
+        sb.append("]");
+        return sb.toString();
     }
+
 
     public String listarTras() {
-        return "";
+        if (isEmpty()) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder("[");
+        NoDuplo<T> atual = this.fim;
+        while (atual != null) {
+            sb.append(atual.getElemento().toString());
+            if (atual.getAnterior() != null) {
+                sb.append(", ");
+            }
+            atual = atual.getAnterior();
+        }
+        sb.append("]");
+        return sb.toString();
     }
-
-    public boolean isEmpty() {
-        return tamanho == 0;
-    }
-
-    public int getTamanho() {
-        return tamanho;
-    }
-
-    private void validarPosicao(int posicao) throws PosicaoInvalidaException {
-
-    }
-
 }
