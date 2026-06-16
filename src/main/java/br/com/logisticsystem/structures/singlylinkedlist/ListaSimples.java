77144b7aeff2cb1424f0d4098c5fee4be9abdc71
simael <simaelsouza2019@gmail.com>
2026-06-16 12:45:34 -0300

    feat: implementa a lista simples generica

--- src/main/java/br/com/logisticsystem/structures/singlylinkedlist/ListaSimples.java
+++ src/main/java/br/com/logisticsystem/structures/singlylinkedlist/ListaSimples.java
@@ -3,56 +3,133 @@ package br.com.logisticsystem.structures.singlylinkedlist;
 import br.com.logisticsystem.exceptions.PosicaoInvalidaException;
 
 public class ListaSimples<T> {
-
-    private NoSimples<T> primeiro;
+    private NoSimples<T> inicio;
     private int tamanho;
 
     public ListaSimples() {
-        this.primeiro = null;
+        this.inicio = null;
         this.tamanho = 0;
     }
 
-    public void inserirInicio(T elemento) {
-
+    public int getTamanho() {
+        return this.tamanho;
     }
 
-    public void inserirFim(T elemento) {
+    public boolean estaVazia() {
+        return this.tamanho == 0;
+    }
 
+    private void validarPosicao(int posicao) {
+        if (posicao < 0 || posicao >= this.tamanho) {
+            throw new PosicaoInvalidaException("Posição inválida: " + posicao + ". Tamanho atual: " + this.tamanho);
+        }
+    }
+
+    private void validarPosicaoInsercao(int posicao) {
+        if (posicao < 0 || posicao > this.tamanho) {
+            throw new PosicaoInvalidaException("Posição de inserção inválida: " + posicao + ". Tamanho atual: " + this.tamanho);
+        }
+    }
+
+    public void inserirNoInicio(T elemento) {
+        NoSimples<T> novoNo = new NoSimples<>(elemento);
+        novoNo.setProximo(this.inicio);
+        this.inicio = novoNo;
+        this.tamanho++;
+    }
+
+    public void inserirNoFim(T elemento) {
+        if (estaVazia()) {
+            inserirNoInicio(elemento);
+            return;
+        }
+
+        NoSimples<T> novoNo = new NoSimples<>(elemento);
+        NoSimples<T> atual = this.inicio;
+
+        while (atual.getProximo() != null) {
+            atual = atual.getProximo();
+        }
+
+        atual.setProximo(novoNo);
+        this.tamanho++;
     }
 
     public void inserir(int posicao, T elemento) {
+        validarPosicaoInsercao(posicao);
 
-    }
+        if (posicao == 0) {
+            inserirNoInicio(elemento);
+        } else if (posicao == this.tamanho) {
+            inserirNoFim(elemento);
+        } else {
+            NoSimples<T> atual = this.inicio;
+            for (int i = 0; i < posicao - 1; i++) {
+                atual = atual.getProximo();
+            }
 
-    public T remover(int posicao) {
-        return null;
+            NoSimples<T> novoNo = new NoSimples<>(elemento, atual.getProximo());
+            atual.setProximo(novoNo);
+            this.tamanho++;
+        }
     }
 
     public T buscar(int posicao) {
-        return null;
+        validarPosicao(posicao);
+
+        NoSimples<T> atual = this.inicio;
+        for (int i = 0; i < posicao; i++) {
+            atual = atual.getProximo();
+        }
+
+        return atual.getElemento();
     }
 
-    public void atualizar(int posicao, T elemento) {
+    public void atualizar(int posicao, T novoElemento) {
+        validarPosicao(posicao);
 
+        NoSimples<T> atual = this.inicio;
+        for (int i = 0; i < posicao; i++) {
+            atual = atual.getProximo();
+        }
+
+        atual.setElemento(novoElemento);
     }
 
-    public String listar() {
-        return "";
+    public T remover(int posicao) {
+        validarPosicao(posicao);
+
+        T elementoRemovido;
+
+        if (posicao == 0) {
+            elementoRemovido = this.inicio.getElemento();
+            this.inicio = this.inicio.getProximo();
+        } else {
+            NoSimples<T> anterior = this.inicio;
+            for (int i = 0; i < posicao - 1; i++) {
+                anterior = anterior.getProximo();
+            }
+
+            NoSimples<T> alvo = anterior.getProximo();
+            elementoRemovido = alvo.getElemento();
+            anterior.setProximo(alvo.getProximo());
+        }
+
+        this.tamanho--;
+        return elementoRemovido;
     }
 
-    public int getTamanho() {
-        return tamanho;
-    }
+    public Object[] listar() {
+        Object[] elementos = new Object[tamanho];
 
-    public boolean isEmpty() {
-        return tamanho == 0;
-    }
+        NoSimples<T> atual = inicio;
+        int indice = 0;
 
-    private void validarPosicao(int posicao) throws PosicaoInvalidaException {
+        while (atual != null) {
+            elementos[indice++] = atual.getElemento();
+            atual = atual.getProximo();
+        }
 
-    }
-
-    private void validarPosicaoInsercao(int posicao) throws PosicaoInvalidaException {
-
+        return elementos;
     }
 }
